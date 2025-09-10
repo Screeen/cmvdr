d7858c8c2d90777f1be789feffd34c0db84db6c0
Giovanni Bologni <giovannibologni@gmail.com>
2025-09-10 17:13:22 +0200

    evaluation can be run from CLI

--- cmvdr/eval/evaluate_folder.py
+++ cmvdr/eval/evaluate_folder.py
@@ -347,24 +347,3 @@ def find_snr_from_filename(file_name):
 
     warnings.warn(f"No SNR found in {file_name}. Returning None.")
     return None  # Return None if no SNR is found
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Evaluate audio files in a folder.")
-    parser.add_argument("-d", "--folder_denoised",
-                        type=str, help="Path to the folder containing denoised audio files.")
-
-    parser.add_argument("-r", "--folder_reference",
-                        type=str, default=None,
-                        help="Path to the folder containing clean reference audio files (optional). ")
-
-    # Add optional bool flag --sort-by-snr with default False
-    parser.add_argument("--sort-by-snr", action='store_true',
-                        help="Sort results by SNR brackets (optional). Default is False.",
-                        default=False)
-
-    args = parser.parse_args()
-    if args.folder_denoised is None:
-        raise ValueError("The folder_denoised argument must be provided.")
-
-    evaluate_audio_files(args.folder_denoised, args.folder_reference, sort_results_by_snr=args.sort_by_snr)
